9881865a83ac3be9c949bb78cfacb0a8759affd7
Tony Zhang <Johnson@dhcp-10-105-227-113.wireless.northwestern.private>
2019-10-03 21:04:28 -0500

    grid created with title

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import 'rbx/index.css';
+import { Button, Container, Title, Column, Notification} from 'rbx';
 
-function App() {
+
+const header = {
+  title: "Wildcat Closet"
+};
+
+const Banner = ({ title }) => (
+  <Title>{ title }</Title>
+);
+
+const Product = ({ product }) => (
+  <Column size="one-quarter">
+    <Notification textAlign="centered">
+      { product.title }
+    </Notification>
+  </Column>
+);
+
+const ProductList = ({ products }) => (
+  <Column.Group vcentered multiline>
+    {products.map(product => <Product key = {product.sku} product = { product } />)}
+  </Column.Group>
+);
+
+
+
+const App = () => {
+  const [data, setData] = useState({});
+  const products = Object.values(data);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch('./data/products.json');
+      const json = await response.json();
+      setData(json);
+    };
+    fetchProducts();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Banner title = {header.title} />
+      <ProductList products = {products} />
+      {/* <ul>
+
+        {products.map(product => <li key={product.sku}>{product.title}</li>)}
+      </ul> */}
+    </Container>
   );
-}
+};
+
+
+
 
 export default App;
